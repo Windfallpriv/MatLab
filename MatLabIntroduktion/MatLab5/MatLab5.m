b41341e56bab63068598962dc11f277f404a35da
Windfallpriv <erikwindfallpriv@gmail.com>
2023-09-26 16:42:00 +0200

    Uppgift 4,5
    Båda färdiga

--- MatLabIntroduktion/MatLab5/MatLab5.m
+++ MatLabIntroduktion/MatLab5/MatLab5.m
@@ -16,10 +16,33 @@ if (f(a)*f(b))<0
         a=(f(a)+f(b))/2;
     end
 end
-a
-b
+
 %% Uppgift 4
 clear, clc
 f=@(x)(x-2.5).*exp(-0.5*(x-2).^2)+0.2;
-x=linspace(-2,7);
-x1=min_bisect(f,[2,3],1.1)
+x=linspace(-2,7,1000);
+
+x23=min_bisect(f,[2,3],0.00001);
+x10=min_bisect(f,[-1,0],0.00001);
+
+
+plot(x,f(x)), grid on
+hold on
+xline(x23)
+hold on
+xline(x10)
+yline(0,'green')
+format short
+text(3,-.15,"Nollställen: " + x10 + " och " + x23)
+%% Uppgift 5
+clear, clc
+x=linspace(0,15);
+
+y=@(x) kastbana2(x);
+plot(x,y(x))
+yline(0)
+axis([-2 14 -2 6])
+
+nollp=min_bisect(y,[5,15],0.01)
+xline(nollp)
+text(9,-1,"Nollpunkten är " + nollp)
