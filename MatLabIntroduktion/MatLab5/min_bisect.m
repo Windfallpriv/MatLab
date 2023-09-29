7a6c7030b85def0cd57437238691721328930b3b
Windfallpriv <erikwindfallpriv@gmail.com>
2023-09-29 16:39:42 +0200

    Update min_bisect.m

--- MatLabIntroduktion/MatLab5/min_bisect.m
+++ MatLabIntroduktion/MatLab5/min_bisect.m
@@ -1,6 +1,22 @@
 function m=min_bisect(f,I,tol)
 % Uppgift 3
-	   
+% min_bisect - beräknar nollställe till f(x) på intervallet I.
+%   Syntax:
+%           x = min_bisect(f,I,tol)
+%   Argument:
+%           f   - funktionshandtag: pekar på namnet till en funktionsfil eller
+%                 till en anonym funktion. T.ex. f=@funk eller f=@(x)cos(x)-x
+%           I   - 1x2 matris, anger ett intervall I=[a,b]. Funktionen måste 
+%                 växla tecken på intervallet.
+%           tol - positivt tal som anger önskad noggrannhet för nollstället.
+%   Returnerar:
+%           x   - ett tal som ger approximativt nollställe.
+%   Beskrivning:
+%           Programmet beräknar ett approximativt nollställe till f(x) på
+%           intervallet I med intervallhalveringsmetoden.
+%   Exempel:
+%           x = min_bisect(@(x)cos(x)-x,[0,1],1e-5)
+
     a=I(1);
     b=I(2);
     c=1;
