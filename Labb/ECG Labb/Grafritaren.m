c0698bc232590274f08033712ac484b58d50b3ee
Windfallpriv <erikwindfallpriv@gmail.com>
2023-10-05 11:05:12 +0200

    Börjat på Labb
    Ritar inte en graf

---
+++ Labb/ECG Labb/Grafritaren.m
@@ -0,0 +1,7 @@
+clear, clc
+Hz=50;
+rawTable = readtable('ECG_Mans.xlsx');
+x = rawTable.Time; %: get the excel column, Header1 (header name)
+y = rawTable.Amplitude; %: get the excel column, Header2 (header name)
+figure;
+plot(x,y); 
