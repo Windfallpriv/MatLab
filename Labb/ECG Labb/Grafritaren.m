a0f310f9a358ec5ac6b5c0c62fcaa01189f933ee
Windfallpriv <erikwindfallpriv@gmail.com>
2023-10-15 20:22:43 +0200

    Update Grafritaren.m

--- Labb/ECG Labb/Grafritaren.m
+++ Labb/ECG Labb/Grafritaren.m
@@ -4,5 +4,8 @@ rawTable = readtable('ECG_Mans.xlsx');
 x = rawTable.Tid; %: get the excel column, Header1 (header name)
 y = rawTable.Amplitude; %: get the excel column, Header2 (header name)
 figure;
-plot(x,y); 
+plot(x,y);, grid on
+xlabel('Tid(s*50)')
+ylabel('Potentialskillnad(V)')
+title('EKG')
 
